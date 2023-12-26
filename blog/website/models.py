140b1cbd39ee9902360df0da4f1adb60dd3039f3
lunamoonsun <2021110052@app.asces.edu.br>
2023-12-26 12:50:09 -0300

    Aula 2 sobre comandos do sistema de template

--- blog/website/models.py
+++ blog/website/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100) # Título do post
     sub_title = models.CharField(max_length=200) # Sobrenome do autor
     content = models.TextField() # Conteúdo do post
+    
 
     # Toda vez que criar um modelo, tem que criar uma migração e depois aplicar a migração para o banco de dados
     # python manage.py makemigrations   
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render # Importa a função render que renderiza o template
+from .models import Post # Importa o model Post que está dentro do aquivo models.py 
 
 def hello_blog(request):
     lista = ['Django', 'Python', 'Git', 'Html', 'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi', 'Systemctl'] # Cria uma lista
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    list_posts = Post.objects.all() # Cria uma lista com todos os objetos do model Post, que busca os posts dentro do banco de dados
+
+   
+    data = {'name': 'Curso de Django 3', 
+            'lista_tecnologias': lista, 
+            'posts': list_posts
+            }
+
+
+
     return render(request, 'index.html', data) # Retorna o index.html que é um template que está dentro da pasta templates que está dentro da pasta website que está dentro da pasta blog
