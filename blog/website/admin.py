052b94dd32692a778dcda6bc0d734580e4d3fe1d
lunamoonsun <2021110052@app.asces.edu.br>
2023-12-26 17:40:58 -0300

    Aula sobre comandos do sistema de template

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -4,6 +4,21 @@ from .models import Post # Importa o modelo Post que foi criado no arquivo model
 # Register your models here.
 # Registra os models para que eles apareçam no painel de administração do django
 
-admin.site.register(Post) # Registra o modelo Post para que ele apareça no painel de administração do django
+# admin.site.register(Post) # Registra o modelo Post para que ele apareça no painel de administração do django
                             # A partir disso pode criar um novo post no painel de administração do django
                         # E será gravado no banco de dados
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'sub_title', 
+        'full_name', 'categories', 
+        'deleted'] # Aqui indica que, (deseja que mostre o título e o sub_título)
+    
+    search_fields = ['title', 'sub_title'] # Aqui indica um campo de pesquisa para aquela página 
+    # fields = ('title', 'sub_title')
+
+
+    def get_queryset(self, request):
+        return Post.objects.filter(deleted=False) # Aqui indica que, (deseja que mostre apenas os que não estão deletados 
+
+admin.site.register(Post, PostAdmin) # Aqui indica que, (deseja que poste o post, igual a classe definida por PostAdmin)
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,13 +1,33 @@
 from django.db import models
 
-# Create your models here.
-# O blog pessoal tem post
+class Categorias(models.TextChoices):
+     TECH = 'TC', 'Tecnologia'
+     CR = 'CR', 'Curiosidade' 
+     GR = 'GR', 'Geral'
 
+
+# Create your models here
+# O blog pessoal tem post
 class Post(models.Model):
     title = models.CharField(max_length=100) # Título do post
     sub_title = models.CharField(max_length=200) # Sobrenome do autor
     content = models.TextField() # Conteúdo do post
+
+    categories = models.CharField( # Define as categorias do post
+        max_length=2,
+        choices=Categorias.choices,
+        default=Categorias.GR,
+    )
+
+    deleted = models.BooleanField(default=True) # Aprovação do post, vai ter um checkbox no painel de administração do django 
+
+    def __str__(self):
+        return self.title
     
+    def full_name(self): #  Juncao do titulo e subtitulo em uma nova coluna
+        return self.title + self.sub_title
+    
+    full_name.admin_order_field  = 'title' # Ordernação baseado no título
 
     # Toda vez que criar um modelo, tem que criar uma migração e depois aplicar a migração para o banco de dados
     # python manage.py makemigrations   
